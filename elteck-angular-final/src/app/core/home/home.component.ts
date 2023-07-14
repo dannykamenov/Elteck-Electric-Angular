8a14196867beff4bb791467e5a9a81755b890f00
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-14 14:24:31 +0300

    added dynamic classes

--- elteck-angular-final/src/app/core/home/home.component.ts
+++ elteck-angular-final/src/app/core/home/home.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
-
-  
-
+  mouseOvered: boolean = false;
+  mouseOvered1: boolean = false;
+  mouseOvered2: boolean = false;
+  mouseOvered3: boolean = false;
+  mouseOvered4: boolean = false;
+  mouseOvered5: boolean = false;
 }
