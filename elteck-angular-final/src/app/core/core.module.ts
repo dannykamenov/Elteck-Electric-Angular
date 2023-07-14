c565dba606a8b53fe77fc00e6eb2cef23b40b72d
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-14 13:15:13 +0300

    fixed header and added assets

--- elteck-angular-final/src/app/core/core.module.ts
+++ elteck-angular-final/src/app/core/core.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { InfographComponent } from './infograph/infograph.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    InfographComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    InfographComponent
   ]
 })
 export class CoreModule { }
