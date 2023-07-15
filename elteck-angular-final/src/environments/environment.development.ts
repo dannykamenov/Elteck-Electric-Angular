bff78381f158cb32726291a3a8cb0db0b6af2407
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-15 17:56:13 +0300

    Update environment.development.ts

--- elteck-angular-final/src/environments/environment.development.ts
+++ elteck-angular-final/src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDImzh6bwGQ7WpQxfpjFLn0AGh4SvpvghY",
+      authDomain: "elteck-angular-softuni.firebaseapp.com",
+      databaseURL: "https://elteck-angular-softuni-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "elteck-angular-softuni",
+      storageBucket: "elteck-angular-softuni.appspot.com",
+      messagingSenderId: "736286200014",
+      appId: "1:736286200014:web:995360e9bb818ad9f8c09f",
+      measurementId: "G-SZ1TX5263J"
+    },
+};
