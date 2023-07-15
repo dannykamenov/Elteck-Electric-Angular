ec8349d0508434000a55f033fba376f27b1bc434
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-15 12:52:27 +0300

    added register with firebase

--- elteck-angular-final/src/app/auth/login/login.component.ts
+++ elteck-angular-final/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Auth } from '@angular/fire/auth'
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private auth: Auth) { 
+    this.loginForm = this.formBuilder.group({
+      email: formBuilder.control('', [Validators.required, Validators.email, Validators.minLength(6)]),
+      pass: formBuilder.control('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  public onSubmit() {
+    
+  }
 
 }
--- elteck-angular-final/src/app/auth/register/register.component.ts
+++ elteck-angular-final/src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { matchPasswordGroup } from 'src/app/shared/validators/match-password-validator';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth'
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent {
 
   registerForm!: FormGroup
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private auth: Auth) { 
     this.registerForm = this.formBuilder.group({
       email: formBuilder.control('', [Validators.required, Validators.email, Validators.minLength(6)]),
       pass: this.formBuilder.group({
@@ -26,7 +27,15 @@ export class RegisterComponent {
   }
 
   public onSubmit() {
-    console.log(this.registerForm.value)
+    if(this.registerForm.invalid) {
+      return
+    }
+    createUserWithEmailAndPassword(this.auth, this.registerForm.value.email, this.registerForm.value.pass.password)
+      .then((res:any) => {
+        console.log(res)
+      }).catch((err) => {
+        alert(err.message)
+      })
   }
 
 }
