62a0ddbe1c0a5833a1f787267c423795cd2c2388
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-19 12:41:54 +0300

    added storage imports

--- elteck-angular-final/src/app/auth/my-profile/my-profile.component.ts
+++ elteck-angular-final/src/app/auth/my-profile/my-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { getAuth, updateProfile } from 'firebase/auth';
+import { Storage, ref, uploadBytesResumable, getDownloadURL } from '@angular/fire/storage'
 
 @Component({
   selector: 'app-my-profile',
@@ -9,13 +10,12 @@ import { getAuth, updateProfile } from 'firebase/auth';
 })
 export class MyProfileComponent {
   isUpdating: boolean = false;
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, public storage: Storage) {}
   public file: any
 
   changeDisplay() {
     this.isUpdating = true;
     const auth = getAuth();
-    console.log(auth.currentUser)
   }
 
   detectFiles(event: any) {
@@ -23,12 +23,18 @@ export class MyProfileComponent {
   }
 
   updateProfile(name: any) {
+
+    if(this.file) {
+      
+    }
+
+
     const auth = getAuth();
 /*     updateProfile(auth.currentUser!, {
       displayName: name? name : auth.currentUser?.displayName,
       photoURL: photoUrl? photoUrl : auth.currentUser?.photoURL,
     }); */
     this.isUpdating = false;
-
+    this.file = null;
   }
 }
