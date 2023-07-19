292c5a600c28a79f7112f0609e1a83a4e572f0b1
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-19 22:13:52 +0300

    added firebasestorage service

---
+++ elteck-angular-final/src/app/shared/services/firebase-storage.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseStorageService {
+
+  constructor() { }
+}
