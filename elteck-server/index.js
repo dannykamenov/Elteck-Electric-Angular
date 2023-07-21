5c453a72100a79b34edcae52aded68c2330676dc
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-22 00:27:41 +0300

    server updates

--- elteck-server/index.js
+++ elteck-server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./mongodb');
+const mongo_uri = config.url;
 
 // Allow requests from any origin - adjust to your needs
 app.use(cors());
-
+console.log(mongo_uri)
 // Your other middleware and route setup goes here...
-
+mongoose.connect(`${mongo_uri}`);
 // Start the server
 const port = 3000; // Replace with your desired port number
 app.listen(port, () => {
