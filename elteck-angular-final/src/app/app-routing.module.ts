4300effedbee1cf16a3b57db644d49042f0b6df1
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-21 22:45:33 +0300

    added logged in guards

--- elteck-angular-final/src/app/app-routing.module.ts
+++ elteck-angular-final/src/app/app-routing.module.ts
@@ -10,16 +10,17 @@ import { VerifyEmailComponent } from './auth/verify-email/verify-email.component
 import { AuthGuard } from './shared/guard/auth.guard';
 import { MyProfileComponent } from './auth/my-profile/my-profile.component';
 import { GalleryComponent } from './elteck/gallery/gallery.component';
+import { AuthGuard2 } from './shared/guard/verified.guard'
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent, data: {title: 'Home'}},
   {path: 'about', component: AboutComponent, data: {title: 'About'}},
   {path: 'my-profile', component: MyProfileComponent, data: {title: 'My Profile'}, canActivate: [AuthGuard]},
   {path: 'contact', component: ContactComponent, data: {title: 'Contact Us'}, canActivate: [AuthGuard]},
-  {path: 'login', component: LoginComponent, data: {title: 'Login'}},
-  {path: 'register', component: RegisterComponent, data: {title: 'Register'}},
-  {path: 'forgot-password', component: ForgotPasswordComponent, data: {title: 'Forgot Password'}},
-  {path: 'verify-email', component: VerifyEmailComponent, data: {title: 'Verify Email'}},
+  {path: 'login', component: LoginComponent, data: {title: 'Login'}, canActivate: [AuthGuard2]},
+  {path: 'register', component: RegisterComponent, data: {title: 'Register'}, canActivate: [AuthGuard2]},
+  {path: 'forgot-password', component: ForgotPasswordComponent, data: {title: 'Forgot Password'}, canActivate: [AuthGuard2]},
+  {path: 'verify-email', component: VerifyEmailComponent, data: {title: 'Verify Email'}, canActivate: [AuthGuard2]},
   {path: 'gallery', component: GalleryComponent, data: {title: 'Gallery'}, canActivate: [AuthGuard]},
 ];
 
