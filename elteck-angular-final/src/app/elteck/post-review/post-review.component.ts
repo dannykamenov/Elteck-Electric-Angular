2d1b5b388982fa3120a36431684df528d0fb18af
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-23 00:39:26 +0300

    added star rating ux

--- elteck-angular-final/src/app/elteck/post-review/post-review.component.ts
+++ elteck-angular-final/src/app/elteck/post-review/post-review.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-review.component.scss']
 })
 export class PostReviewComponent {
+  stars = [1, 2, 3, 4, 5];
+  rating = 0;
+
+  mouseEnterHandler(index: number) {
+    this.rating = index;
+  }
+
+  mouseLeaveHandler() {
+    this.rating = 0;
+  }
 
 }
