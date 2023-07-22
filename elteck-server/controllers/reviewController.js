fcb6193dcafce29c2300895e0f6acd2321c809cb
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-22 16:16:01 +0300

    added create review

--- elteck-server/controllers/reviewController.js
+++ elteck-server/controllers/reviewController.js
@@ -1,27 +1,50 @@
-const Review = require('../models/reviewModel');
-
+const Review = require("../models/reviewModel");
 
 function getReviews(req, res) {
-    Review.find({}, (err, reviews) => {
-        if (err) {
-            res.status(500).json({ error: err });
-        }
-        res.status(200).json(reviews);
-    });
+  Review.find({}, (err, reviews) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    }
+    res.status(200).json(reviews);
+  });
 }
 
 function getLatestReviews(req, res) {
-    const limit = Number(req.query.limit) || 0;
+  const limit = Number(req.query.limit) || 0;
 
-    Review.find().sort({ created_at: -1 }).limit(limit).then((reviews) => {
-        res.status(200).json(reviews);
-    }).catch((err) => {
-        res.status(500).json({ error: err });
+  Review.find()
+    .sort({ created_at: -1 })
+    .limit(limit)
+    .then((reviews) => {
+      res.status(200).json(reviews);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
     });
 }
 
+async function postReview(req, res) {
+    const { uid, title, content, rating, date, username, useremail, userimage } = req.body;
+    try {
+        const review = await Review.create({
+            uid,
+            title,
+            content,
+            rating,
+            date,
+            username,
+            useremail,
+            userimage,
+        });
+        res.status(201).json(review);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+}
+
 module.exports = {
-    getReviews,
-    getLatestReviews,
+  getReviews,
+  getLatestReviews,
+  postReview,
 
 };
--- elteck-server/router/router.js
+++ elteck-server/router/router.js
@@ -1,9 +1,10 @@
-const { getReviews, getLatestReviews } = require('../controllers/reviewController');
+const { getReviews, getLatestReviews, postReview } = require('../controllers/reviewController');
 
 const router = require('express').Router();
 
 router.get('/reviews', getReviews);
 router.get('/latest', getLatestReviews);
+router.post('/reviews', postReview)
 
 
 module.exports = router;
