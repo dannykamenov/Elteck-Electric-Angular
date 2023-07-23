b20f00178499ea273d2e793a5da155ebb4f1a42d
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-23 12:11:30 +0300

    added review interface

---
+++ elteck-angular-final/src/app/shared/services/review.ts
@@ -0,0 +1,10 @@
+export interface Review {
+    uid: string;
+    title: string;
+    content: string;
+    rating: number;
+    username: string;
+    useremail: string;
+    userimage: string;
+
+}
