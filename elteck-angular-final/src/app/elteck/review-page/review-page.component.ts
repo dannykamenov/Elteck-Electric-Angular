3d347a9835ac37b62396d772d87869a961fa1375
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-24 19:43:59 +0300

    added review page styling

--- elteck-angular-final/src/app/elteck/review-page/review-page.component.ts
+++ elteck-angular-final/src/app/elteck/review-page/review-page.component.ts
@@ -8,12 +8,16 @@ import { getReview } from 'src/app/shared/services/getReview';
   styleUrls: ['./review-page.component.scss']
 })
 export class ReviewPageComponent {
+  isLoading = true;
+  isNotLoading = false;
   reviews: undefined | getReview[];
 
     constructor(private api: ApiService) { 
       this.api.getReviews().subscribe((res) => {
         this.reviews = res;
         console.log(this.reviews)
+        this.isNotLoading = true;
+        setTimeout(() => {this.isLoading = false}, 1000);
       });
     }
 }
