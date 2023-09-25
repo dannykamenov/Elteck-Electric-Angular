928b069a40c37926894c054d3e95073875b5f02e
Daniel Kamenov <sorostmcoc@gmail.com>
2023-09-25 14:14:29 +0300

    updated server logic

--- elteck-admin-panel/index.js
+++ elteck-admin-panel/index.js
@@ -1,9 +1,23 @@
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const loginBtn = document.getElementById('loginBtn');
-const email = document.getElementById('email');
+const form = document.getElementById('login-form');
 
-loginBtn.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    console.log('login');
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get the form data
+    const formData = new FormData(form);
+
+    // Send the data to the server using the fetch API
+    fetch('your-server-url', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json()) // If you expect JSON response from the server
+    .then(data => {
+        // Handle the server's response here (e.g., display a message)
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('Error:', error);
+    });
 });
