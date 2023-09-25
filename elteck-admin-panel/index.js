ef70f7bf6e23551a1db87fc79948c61d669fafd3
Daniel Kamenov <sorostmcoc@gmail.com>
2023-09-25 18:08:31 +0300

    fix

--- elteck-admin-panel/index.js
+++ elteck-admin-panel/index.js
@@ -1,15 +1,17 @@
 const form = document.getElementById('login-form');
 
+
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get the form data
-    const formData = new FormData(form);
-
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     // Send the data to the server using the fetch API
     fetch('https://elteck-server.onrender.com/api/admin', {
         method: 'POST',
-        body: formData
+        body: JSON.stringify({ username, email, password }),
     })
     .then(response => response.json()) // If you expect JSON response from the server
     .then(data => {
--- elteck-server/controllers/adminDashboard.js
+++ elteck-server/controllers/adminDashboard.js
@@ -3,6 +3,7 @@ const Review = require("../models/reviewModel");
 function adminDashboard(req, res) {
     const { username, email, password } = req.body;
     console.log(req.body);
+    console.log(req)
     if (username === 'admin' && email === 'test@123.com' && password === 'testadmin') {
         res.status(200).json({ message: 'Success' });
     }
