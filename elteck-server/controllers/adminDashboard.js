e8af742fdac6777bb0ba75d2f1f51aafed233f11
Daniel Kamenov <sorostmcoc@gmail.com>
2023-09-25 15:52:07 +0300

    Update adminDashboard.js

--- elteck-server/controllers/adminDashboard.js
+++ elteck-server/controllers/adminDashboard.js
@@ -2,6 +2,9 @@ const Review = require("../models/reviewModel");
 
 function adminDashboard(req, res) {
     const { username, email, password } = req.body;
+    if (username === 'admin' && email === 'test@123.com' && password === 'testadmin') {
+        res.status(200).json({ message: 'Success' });
+    }
 }
 
 module.exports = adminDashboard;
