e57d6a440c0e04b73209e72e2ddc8494327141a4
Daniel Kamenov <sorostmcoc@gmail.com>
2023-09-25 19:50:43 +0300

    added auth

--- elteck-admin-panel/index.js
+++ elteck-admin-panel/index.js
@@ -11,16 +11,33 @@ form.addEventListener('submit', function(event) {
     // Send the data to the server using the fetch API
     fetch('https://elteck-server.onrender.com/api/admin', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
         body: JSON.stringify({ username, email, password }),
     })
     .then(response => response.json()) // If you expect JSON response from the server
     .then(data => {
         // Handle the server's response here (e.g., display a message)
-        console.log(data);
-        console.log('Success:', data.message);
+        if (data.message === 'Success') {
+            localStorage.setItem('admin', 'true');
+            location.reload();
+        }
     })
     .catch(error => {
         // Handle any errors that occurred during the fetch
         console.error('Error:', error);
     });
 });
+
+const admin = localStorage.getItem('admin');
+const loginContainer = document.querySelector('.login-container');
+const dashboard = document.querySelector('.dashboard');
+if (admin === 'true') {
+    loginContainer.style.display = 'none';
+    dashboard.style.display = 'block';
+} else {
+    loginContainer.style.display = 'block';
+    dashboard.style.display = 'none';
+}
+
